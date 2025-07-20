daed7aec887b794321f8542bf59857763834b002
rohan09s <rohansabnis9@gmail.com>
2025-07-20 23:24:28 +0530

    Update categoryModel.js

--- backend/models/categoryModel.js
+++ backend/models/categoryModel.js
@@ -11,27 +11,3 @@ let categorySchema=new mongoose.Schema({
     }
 },{timestamps:true})
 module.exports=mongoose.model("categories",categorySchema)
-
-
-
-
-
-
-
-
-
-
-// const mongoose= require('mongoose')
-// let categorySchema= new mongoose.Schema({
-//     name:{
-//         type: String,
-//         required:true,
-//         unique:true
-//     },
-//     slug:{
-//         type:String,
-//         lowercase:true
-//     }
-// },{timestamps:true})
-
-// module.exports=mongoose.model("categories",categorySchema)
