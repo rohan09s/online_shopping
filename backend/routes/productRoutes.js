0462c67a7d15758ad02b1aeac88cb133a6d76e86
rohan09s <rohansabnis9@gmail.com>
2025-07-20 23:27:10 +0530

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -16,32 +16,3 @@ router.delete("/delete-product/:pid",requireSignIn,isAdmin,deleteProductControll
 router.get("/search-product/:key",searchProductController)
 router.post("/filter-product",filterProductController)
 module.exports=router
-
-
-
-
-// const express= require('express')
-// const formidable= require('express-formidable')
-// const { createProductController, getAllProductsController, getSingleProductController, getProductPhotoController, updateProductController, deleteProductController, searchProductController, filterProductController } = require('../controllers/productControllers')
-// const { requireSignIn, isAdmin } = require('../middlewares/authMiddlewares')
-// const router= express.Router()
-
-// router.post("/create-product",requireSignIn,isAdmin,formidable(),createProductController)
-// router.get("/all-products",getAllProductsController)
-// router.get("/single-product/:slug",getSingleProductController)
-// router.get("/product-photo/:pid",getProductPhotoController)
-// router.put("/update-product/:pid",requireSignIn,isAdmin,formidable(),updateProductController)
-// router.delete("/delete-product/:pid",requireSignIn,isAdmin,deleteProductController)
-// router.get("/search-product/:key",searchProductController)
-// router.get("/filter-product",filterProductController)
-
-
-// module.exports= router
-
-
-
-
-
-
-
-// // Use post method to create, put method to update, get method to get/display the data and search the data and to filter the data, delete method to delete the data.
